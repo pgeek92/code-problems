70881afea9897af22880ef3f9366d232c98eae7e
Prashant Gupta <prashantgupta1061992@gmail.com>
2018-05-19 14:40:50 +0530

    Missed adding the Bugs code in the previous check in, Adding now

--- coding-problems/src/challenges/trustingSocial/Bugs.java
+++ coding-problems/src/challenges/trustingSocial/Bugs.java
@@ -85,6 +85,29 @@
 
 package challenges.trustingSocial;
 
-public class Bugs {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
 
+public class Bugs {
+	public static void main(String args[] ) throws Exception {
+        //BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<Integer> arrayList = new ArrayList<Integer>();
+        for(int i = 0; i < N; i++){
+            String[] input = br.readLine().split(" ");
+            if(Integer.parseInt(input[0]) == 1){
+                arrayList.add(Integer.parseInt(input[1]));
+            }
+            if(Integer.parseInt(input[0]) == 2){
+                Collections.sort(arrayList, Collections.reverseOrder());
+                if(arrayList.size() < 3){
+                    System.out.println("Not enough enemies");
+                }else{
+                    System.out.println(arrayList.get((arrayList.size()/3)-1));
+                }
+            }
+        }
+    }
 }
