a77eb1ddaee93a7e4ebae95ae384769f4761f808
Prashant Gupta <prashantgupta1061992@gmail.com>
2020-08-19 18:34:11 +0530

    Added problem description for HappyNumber

--- coding-problems/src/leetcode/HappyNumber.java
+++ coding-problems/src/leetcode/HappyNumber.java
@@ -1,4 +1,22 @@
 /**
+ * A happy number is a number defined by the following process: 
+ * Starting with any positive integer, replace the number by the 
+ * sum of the squares of its digits, and repeat the process until 
+ * the number equals 1 (where it will stay), or it loops endlessly in 
+ * a cycle which does not include 1. Those numbers for which this process 
+ * ends in 1 are happy numbers.
+ * 
+ * Return True if n is a happy number, and False if not.
+ * 
+ * Example: 
+ * 
+ * Input: 19
+ * Output: true
+ * Explanation: 
+ * 1*1 + 9*9 = 82
+ * 8*8 + 2*2 = 68
+ * 6*6 + 8*8 = 100
+ * 1*1 + 0*0 + 0*0 = 1
  * 
  */
 package leetcode;
